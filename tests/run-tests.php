0221489a8aaf5aa262b255b50798f10c4422da2a
Jakub Trmota <jakub@trmota.cz>
2025-01-03 00:40:46 +0100

    Tests: add basic test

---
+++ tests/run-tests.php
@@ -0,0 +1,29 @@
+#!/bin/php
+<?php declare(strict_types=1);
+
+$tracyLogFile = __DIR__ . '/logs/tracy-remote-bar.log';
+
+@unlink($tracyLogFile); // intentionally @ - file may not exists
+
+if (exec('(cd ' . __DIR__ . ' && ./test-cli-request.php)', $output, $exitCode) === FALSE) {
+	echo 'An error occurred running test (exit code ' . $exitCode . '): ' . PHP_EOL . implode(PHP_EOL, $output) . PHP_EOL;
+	exit(1);
+}
+
+if (!is_file($tracyLogFile)) {
+	echo 'There is no tracy-remote-bar.log file, Tracy is probably not correctly patched.' . PHP_EOL;
+	exit(2);
+}
+
+$log = file_get_contents($tracyLogFile);
+if ($log === FALSE) {
+	echo 'Can\'t load data from tracy-remote-bar.log.' . PHP_EOL;
+	exit(3);
+}
+
+if (!str_contains($log, '#28: Connection timed out after')) {
+	echo 'There is missing info about timed out connection in tracy-remote-bar.log. Log data:' . PHP_EOL . $log . PHP_EOL;
+	exit(4);
+}
+
+echo 'OK' . PHP_EOL;
